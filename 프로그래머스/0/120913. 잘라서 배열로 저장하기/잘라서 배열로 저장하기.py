d72fdcad2f9de103a5e232b90e398cf3f32c481f
Seoyeon Lee <skylarlee1003@gmail.com>
2026-05-02 09:33:08 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,7 +1,7 @@
 def solution(my_str, n):
     answer = []
     # my_str 을 n 만큼씩 잘라서 answer에 담기
-        # 처음부터 n번 반복, my_str이 끝날때까지  
+        # 처음부터 n 간격으로 이동, my_str이 끝날때까지  
         # 자른 문자열 answer 배열에 할당 
     for i in range(0, len(my_str), n):
         sliced_str = my_str[i:n + i]
@@ -9,5 +9,3 @@ def solution(my_str, n):
     
     
     return answer
-
-
